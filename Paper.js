7db4bb8048d1d53319473cccb11ea9fc25af79b4
manasvi223 <74662324+manasvi223@users.noreply.github.com>
2021-03-24 13:36:19 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,17 @@
+Class Paper {
+constructor(x,y,width,height) 
+var options = {
+    isStatic:false,
+    'restitution':0.3,
+    'friction':0,
+    'density':1.
+}
+this.image = loadImage("sprites/paper.png");
+
+
+
+display()
+this.body.position.x = mouseX;
+this.bdy.position.y = mouseY;
+} 
+  
